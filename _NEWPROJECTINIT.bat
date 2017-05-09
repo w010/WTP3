f08d8064abb62780da0c40758489b647de5fdf43
A. wolo Wolski <wolo.wolski@gmail.com>
2017-05-10 00:44:21 +0200

    _NEWPROJECTINIT v04

--- _NEWPROJECTINIT.bat
+++ _NEWPROJECTINIT.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
 
-rem WTP - NEW PROJECT INIT v.02
+rem WTP - NEW PROJECT INIT v0.4
 
 rem ('rem' is a comment in bat)
 rem echo Current dir = %CD%
@@ -12,5 +12,6 @@ xcopy D:\WORK\___projects\WTP3\_makelinks.bat
 xcopy D:\WORK\___projects\WTP3\dynamicReturnTypeMeta.json
 xcopy D:\WORK\___projects\WTP3\typocli.ps1
 xcopy D:\WORK\___projects\WTP3\typocli.sh
+xcopy D:\WORK\___projects\WTP3\typocli.bat
 xcopy D:\WORK\___projects\WTP3\typo_clearcache.bat
 xcopy D:\WORK\___projects\WTP3\typo_installfile.bat
---
+++ typocli.bat
@@ -0,0 +1,2 @@
+
+"D:\xampp\php5.5.38\php.exe" "typo3\cli_dispatch.phpsh" %1 %2 %3 %4
