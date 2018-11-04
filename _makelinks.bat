c448dc87f58c900146adb8805edc4503ba9577d8
A. wolo Wolski <wolo.wolski@gmail.com>
2018-11-04 23:06:33 +0100

    updated some wtp system scripts

--- _makelinks.bat
+++ _makelinks.bat
@@ -1,4 +1,7 @@
 cd /d %~dp0
+
+rem WTP - NEW PROJECT INIT: makelinks v0.2
+
 rem mklink /D __CONTENTS 	fileadmin\contents\
 rem mklink /D __IMAGES 	fileadmin\templates\default\images\
 rem mklink /D __TEMPLATES 	fileadmin\templates\default\
@@ -9,5 +12,5 @@ rem mklink index.php 		typo3_src\index.php
 
 
 mkdir "F:\dropbox\WORK files\res - WPS - WTP3"
-mklink /D ..\_res_dbox "F:\dropbox\WORK files\res - WPS - WTP3"
+mklink /D ..\_res "F:\dropbox\WORK files\res - WPS - WTP3"
 pause
--- t3cli_docker.sh
+++ t3cli_docker.sh
@@ -4,4 +4,7 @@
 
 docker exec -it malerpraxisdev_php_1 php /var/www/htdocs/typo3/cli_dispatch.phpsh $1 $2 $3 $4
 
+#_docker/php_proxy.sh php /var/www/trunk/typo3/cli_dispatch.phpsh extbase help
+_docker/php_proxy.sh php /var/www/htdocs/typo3/cli_dispatch.phpsh extbase help
+
 # read -p "Press enter to continue" nothing
